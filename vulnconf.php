c3e9d425a4cf59378090253682f478ca1bd53b4e
Superbug <evren@superbug.co>
2016-02-23 18:10:00 +0200

    Update vulnconf.php

--- vulnconf.php
+++ vulnconf.php
@@ -2,7 +2,7 @@
 	ini_set('display_errors', 0);
 	ini_set('display_startup_errors', 0);
 	require "goygoy.php";
-	$link = mysqli_connect("127.0.0.1", $u, $p, "old");
+	$link = mysqli_connect("127.0.0.1", $u, $p, $d);
 	mysqli_set_charset($link, 'utf8'); 
 	mysqli_query($link, "SET NAMES 'utf8';");
 	mysqli_query($link, "SET CHARACTER SET 'utf8';");
